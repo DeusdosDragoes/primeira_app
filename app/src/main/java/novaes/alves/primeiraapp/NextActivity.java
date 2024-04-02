3ae75007764c3c0a3f8770bbb1a0e655822e28dc
arthu <arthur.10042004@gmail.com>
2024-04-02 09:43:51 -0300

    comentarios

--- app/src/main/java/novaes/alves/primeiraapp/NextActivity.java
+++ app/src/main/java/novaes/alves/primeiraapp/NextActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.TextView; //Importes automaticos
 
 public class NextActivity extends AppCompatActivity {
 
